bb57832ebcf9d1892abe81473d7de7fefcf44a0b
ruanqizhen <ruanqizhen@gmail.com>
2021-11-16 20:22:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   // your website's title
-  document_title: "Website tiltle",
+  document_title: "LabVIEW 编程经验",
 
   // index page
   index: "README.md",
@@ -9,7 +9,7 @@ var CONFIG = {
   sidebar_file: "sidebar.md",
 
   // where the docs are actually stored on github - so you can edit
-  base_url: "https://github.com/ruanqizhen/labview_book/blob/main",
+  base_url: "https://github.com/ruanqizhen/labview_book/edit/main",
 };
 
 // **************************
