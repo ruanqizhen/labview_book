a88c54b9e6922e4656a9c7d9d8570769f3da2eff
ruanqizhen <ruanqizhen@gmail.com>
2021-12-28 19:44:00 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -228,6 +228,11 @@ const sidebars = {
 		},
 		{
 		  type: 'link',
+		  label: '北航出版社出版',
+		  href: 'http://service.buaapress.com.cn/mzs/book/detail/id/2624',
+		},
+		{
+		  type: 'link',
 		  label: '作者博客（墙外）',
 		  href: 'https://ruanqizhen.wordpress.com',
 		},
