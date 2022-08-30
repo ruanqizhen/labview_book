7daa05c4cc1b3a82f78ff1721699b8fd7a46a1d5
ruanqizhen <ruanqizhen@gmail.com>
2022-08-30 19:09:04 -0400

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,3 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
- */
-
 // @ts-check
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
@@ -216,6 +205,7 @@ const sidebars = {
       label: '附录',
       items: [
 	    'appendix_problem',
+		'appendix_languages',
 		'appendix_reference', 
 		'appendix_epilogue',
 		{
