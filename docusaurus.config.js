95a17fde2a7dc8cf6fe910ef53e9f233ea40183d
ruanqizhen <ruanqizhen@gmail.com>
2022-09-06 21:44:11 -0400

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,8 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -26,6 +28,8 @@ const config = {
           editUrl: 'https://github.com/ruanqizhen/labview_book/edit/main/',
 		  routeBasePath: '/',
 		  path: './docs',
+		  remarkPlugins: [math],
+		  rehypePlugins: [katex],
         },
 		blog: false,
         theme: {
@@ -98,6 +102,15 @@ const config = {
       },
     ],
     "./src/plugin/plugin-image-zoom",
+  ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 };
 
