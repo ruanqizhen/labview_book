f7745c5656b0e4b90046f79486d33efe63522335
ruanqizhen <ruanqizhen@gmail.com>
2023-11-24 08:02:17 -0500

    Update index.js

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -3,23 +3,26 @@ import DocPaginator from '@theme-original/DocPaginator';
 import Giscus from '@giscus/react';
 
 export default function DocPaginatorWrapper(props) {
+  let giscus_term = location.pathname.split('/').pop();
+  if (giscus_term == '') giscus_term = 'index';
   return (
     <>
       <DocPaginator {...props} />
-	  <br />
-	  <Giscus
-		repo='ruanqizhen/labview_book'
-		repoId='R_kgDOGYjRCQ'
-		category='Announcements'
-		categoryId='DIC_kwDOGYjRCc4B_4dq'
-		mapping='pathname'
-		reactionsEnabled='1'
-		emitMetadata='1'
-		inputPosition='top'
-		theme='light'
-		lang='en'
+      <br />
+      <Giscus
+        repo='ruanqizhen/labview_book'
+        repoId='R_kgDOGYjRCQ'
+        category='Announcements'
+        categoryId='DIC_kwDOGYjRCc4B_4dq'
+        mapping='specific'
+        term={giscus_term}
+        reactionsEnabled='1'
+        emitMetadata='1'
+        inputPosition='top'
+        theme='light'
+        lang='en'
         loading="lazy"
-	  />
+      />
     </>
   );
 }
